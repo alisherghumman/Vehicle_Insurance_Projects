fec2ed85d5bfc7ae690f9bf011f3691fbd78811c
Guts <gutsvulture@protonmail.com>
2025-12-07 18:00:08 +0500

    pushed data to mongodb and added the exception and logging

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Ali Sher",
+    author_email="gutsvulture@protonmail.com",
+    packages=find_packages()
+)
